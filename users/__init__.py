81737b4c6f97cf1e239b029a03bc175d8b732307
Arun Rana <m.xpert786@gmail.com>
2025-11-06 15:12:48 +0530

    Fix: Remove problematic imports from users/__init__.py that cause AppRegistryNotReady

--- users/__init__.py
+++ users/__init__.py
@@ -1,6 +1,1 @@
-# Import models to ensure Django discovers them
-from . import models
-from . import syndicate_document_models
-from . import syndicate_image_models
-
-__all__ = ['models', 'syndicate_document_models', 'syndicate_image_models']
+# Models are auto-discovered by Django, no need to import here
